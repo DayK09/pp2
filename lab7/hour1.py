30b2f47cb7c2905b5337887402856bfa0c53f5bc
DayK09 <156871572+DayK09@users.noreply.github.com>
2024-03-20 02:47:14 +0600

    hour1.py

--- lab7/hour1.py
+++ lab7/hour1.py
@@ -7,37 +7,41 @@ pygame.init()
 screen = pygame.display.set_mode((830, 800))
 pygame.display.set_caption('Clock')
 clock = pygame.time.Clock()
-main = pygame.image.load('lab7/main-clock.png')
-right = pygame.image.load('lab7/right-hand.png')
-left = pygame.image.load('lab7/left-hand.png')
+main = pygame.image.load('main-clock.png')
+right = pygame.image.load('right-hand.png')
+left = pygame.image.load('left-hand.png')
 
-def time():
+def rot_center(image, angle, x, y):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=image.get_rect(center=(x, y)).center)
+    return rotated_image, new_rect
+
+def run_clock():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                exit()
+                return
         
-        ctime = datetime.now()
-        hour, minute = ctime.hour % 12, ctime.minute 
+        current_time = datetime.now()
+        hour = current_time.hour % 12
+        minute = current_time.minute
         
-        # Рассчитываем углы в градусах
-        minute_angle = minute *4 
-        hour_angle = hour * 25 
+        # Calculate angles in degrees
+        minute_angle = minute * 6  # 360 degrees divided by 60 minutes
+        hour_angle = (hour * 30) + (minute / 2)  # 360 degrees divided by 12 hours + (30 degrees divided by 60 minutes)
         
         screen.fill((255, 255, 255))
         screen.blit(main, (0, 0))
         
-        rotated_right = pygame.transform.rotate(right, -hour_angle)
-        right_rect = rotated_right.get_rect(center=(415, 400))
+        rotated_right, right_rect = rot_center(right, -hour_angle + 90, 415, 400)
         screen.blit(rotated_right, right_rect)
        
-        rotated_left = pygame.transform.rotate(left, -minute_angle)
-        left_rect = rotated_left.get_rect(center=(415, 400))
+        rotated_left, left_rect = rot_center(left, -minute_angle + 90, 415, 400)
         screen.blit(rotated_left, left_rect)
 
         pygame.display.update()
         clock.tick(60)
 
-time()
+run_clock()
 pygame.quit()
